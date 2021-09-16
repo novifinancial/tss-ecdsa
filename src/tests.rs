5f31472c7eba2f84ba1fa7de581f72f04b23c6e3
qrypt0 <90406557+qrypt0@users.noreply.github.com>
2021-09-16 11:24:48 +0200

    Attempt to re-organize slightly (#8)
    * Attempt to re-organize slightly
    
    * Fixing clippy issues
    
    Co-authored-by: Ladi de Naurois <ladi@fb.com>
    Co-authored-by: Kevin Lewi <klewi@fb.com>

---
+++ src/tests.rs
@@ -0,0 +1,139 @@
+use super::*;
+
+use ecdsa::signature::DigestVerifier;
+use rand::rngs::OsRng;
+use sha2::{Digest, Sha256};
+
+/// Generate safe primes from a file. Usually, generating safe primes takes
+/// awhile (5-10 minutes per 1024-bit safe prime on my laptop)
+fn get_safe_primes() -> Vec<BigNumber> {
+    let file_contents = std::fs::read_to_string("src/safe_primes.txt").unwrap();
+    let mut safe_primes_str: Vec<&str> = file_contents.split("\n").collect();
+    safe_primes_str = safe_primes_str[0..safe_primes_str.len() - 1].to_vec(); // Remove the last element which is empty
+    let safe_primes: Vec<BigNumber> = safe_primes_str
+        .into_iter()
+        .map(|s| bincode::deserialize(&hex::decode(&s).unwrap()).unwrap())
+        .collect();
+    safe_primes
+}
+
+/// Executes a test between two parties i and j
+#[test]
+fn run_test() {
+    let mut rng = OsRng;
+    let NUM_PARTIES = 3;
+    let safe_primes = get_safe_primes();
+
+    // Keygen
+    println!("Beginning Keygen");
+    let mut keyshares = vec![];
+    for i in 0..NUM_PARTIES {
+        let keyshare =
+            key::KeyShare::from_safe_primes(&mut rng, &safe_primes[2 * i], &safe_primes[2 * i + 1]);
+        keyshares.push(keyshare);
+    }
+
+    // Round 1
+    println!("Beginning Round 1");
+    let mut r1_privs = vec![];
+    let mut r1_pubs = vec![];
+    for k in keyshares.iter() {
+        let key::Pair { private, public } = k.round_one();
+        r1_privs.push(private);
+        r1_pubs.push(public);
+    }
+
+    // Round 2, each step needs to be done for each j != i
+    println!("Beginning Round 2");
+    let mut r2_privs = vec![];
+    let mut r2_pubs = vec![];
+    for i in 0..NUM_PARTIES {
+        let mut r2_priv_i = vec![];
+        let mut r2_pub_i = vec![];
+        for j in 0..NUM_PARTIES {
+            if j == i {
+                r2_priv_i.push(None);
+                r2_pub_i.push(None);
+                continue;
+            }
+            let key::Pair {
+                private: r2_priv_ij,
+                public: r2_pub_ij,
+            } = keyshares[i].round_two(&keyshares[j].public, &r1_privs[i], &r1_pubs[j]);
+            r2_priv_i.push(Some(r2_priv_ij));
+            r2_pub_i.push(Some(r2_pub_ij));
+        }
+        r2_privs.push(r2_priv_i);
+        r2_pubs.push(r2_pub_i);
+    }
+
+    // Round 3, each step needs to be done for each j != i
+    println!("Beginning Round 3");
+    let mut r3_privs = vec![];
+    let mut r3_pubs = vec![];
+    for i in 0..NUM_PARTIES {
+        let r2_pubs_cross = {
+            let mut result = vec![];
+            for j in 0..NUM_PARTIES {
+                result.push(r2_pubs[j][i].clone());
+            }
+            result
+        };
+
+        let key::Pair {
+            private: r3_priv,
+            public: r3_pub,
+        } = keyshares[i].round_three(&r1_privs[i], &r2_privs[i][..], &r2_pubs_cross[..]);
+        r3_privs.push(r3_priv);
+        r3_pubs.push(r3_pub);
+    }
+
+    // Presign Finish
+    println!("Beginning Presign Finish");
+
+    let mut presign_records = vec![];
+    for private in r3_privs.into_iter() {
+        let record_i: PresignRecord = RecordPair {
+            private,
+            public: r3_pubs.clone(),
+        }
+        .into();
+        presign_records.push(record_i);
+    }
+
+    // Produce sign share
+    println!("Produce sign share");
+
+    let mut hasher = Sha256::new();
+    hasher.update(b"hello world");
+
+    let mut signing_key = k256::Scalar::zero();
+    let mut verifying_key = k256::ProjectivePoint::identity();
+    for i in 0..NUM_PARTIES {
+        signing_key += crate::key::bn_to_scalar(&keyshares[i].private.x).unwrap();
+        verifying_key += keyshares[i].public.X;
+    }
+
+    let vk = ecdsa::VerifyingKey::from_encoded_point(&verifying_key.to_affine().into()).unwrap();
+
+    let mut s_acc = k256::Scalar::zero();
+    let mut r_scalars = vec![];
+    for record in presign_records.into_iter() {
+        let (r, s) = record.sign(hasher.clone());
+        s_acc += s;
+        r_scalars.push(r);
+    }
+
+    // All r's should be the same
+    for i in 0..NUM_PARTIES - 1 {
+        assert_eq!(r_scalars[i], r_scalars[i + 1]);
+    }
+
+    if s_acc.is_high().unwrap_u8() == 1 {
+        s_acc = s_acc.negate();
+    }
+
+    let sig = ecdsa::Signature::from_scalars(r_scalars[0], s_acc).unwrap();
+
+    assert!(vk.verify_digest(hasher, &sig).is_ok());
+}
