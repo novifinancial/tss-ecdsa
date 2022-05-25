e6ba09b8506aaa76e7f365dc89c049f9cd10582f
Thomas Yurek <tyurek@fb.com>
2022-05-25 16:41:13 -0700

    update license info

--- src/zkp/setup.rs
+++ src/zkp/setup.rs
@@ -1,7 +1,9 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
 //
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
+// This source code is licensed under both the MIT license found in the
+// LICENSE-MIT file in the root directory of this source tree and the Apache
+// License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+// of this source tree.
 
 //! Generates setup parameters (N, s, t) used for other ZKPs. See
 //! the paragraph before Section 2.3.1 of <https://eprint.iacr.org/2021/060.pdf>
