5f76a1a13e1410dafb49f1ed8f849438ab776ade
Tom Yurek <yurek2@illinois.edu>
2022-10-27 00:20:58 -0400

    improve documentation for benchmarking script

--- dev/flame.py
+++ dev/flame.py
@@ -1,5 +1,26 @@
+# flame.py: a script to reorganize flame-graph.html
+# running this script while a copy of flame-graph.html is found in the same directory
+# will produce a new file called flame-graph-new.html in which all invocations of the
+# same subfunction within a given function are grouped together
+#
+# context: the "flame" rust crate produces flame graphs which can help visualize which
+# parts of the program require the most time to execute. This flamegraph orders functions 
+# chronologically on the x axis, but if the same function is called in multiple places in 
+# the general execution flow, then it will appear broken up into various chunks, making 
+# it hard to tell how much of the overall execution is attributable to that function.
+#
+# This code essentially parses the json in the flame-graph.html file output by the flame 
+# crate, reorganizes it, and writes a new html flame graph which groups together invocations
+# in a more convenient way.
+
+
 import json
 import re
+
+# merges any separate timings of the same function with the same parent together 
+# e.g. if function A() calls C() twice and function B() calls C() four times,
+# this would show A() and C() both calling B() once, but it taking around twice as long in C()
+# (assuming in this example that all calls to C() take roughly the same amount of time)
 def merge(blob):
     if len(blob['children']) == 0:
         return
@@ -15,9 +36,10 @@ def merge(blob):
     blob['children'] = new_children_list
     for new_child_idx in range(len(new_children_list)):
         merge(new_children_list[new_child_idx])
-    
+
+# aligns a function's subfunctions (children) to all be positioned sequentially along the x axis
+# and start at the same time as their parent
 def organize_children(blob):
-    # assumes the children are all uniquely named
     parent_start = blob['start']
     last_end = parent_start
     for child_idx in range(len(blob['children'])):
@@ -27,17 +49,23 @@ def organize_children(blob):
         if len(blob['children'][child_idx]['children']) > 0:
             organize_children(blob['children'][child_idx])
 
+# flame-graph.html is composed of a large json data blob and some code to process and display it
+# this parses out the json part specifically and formats it in a way the python json module can handle
 def parse_html():
     #d3.select("body").datum({ children:
     f = open("flame-graph.html", "r")
     text = "".join(f.readlines())
     
+    # search the file for the specific substring d3.select("body").datum({ children:
+    # everthing between this substring and the last 59 characters of the file is json data
     jsonish_re = re.search('d3\.select\("body"\)\.datum\(\{ children: ', text)
     span = jsonish_re.span()
     head = text[0:span[1]]
     jsonish = text[span[1]:-59]
     tail = text[-59:]
     
+    # the "json-ish" text from the html file needs to be modified slightly so that
+    # the python json parser can handle it
     jsonish = jsonish.replace('name:', '"name":')
     jsonish = jsonish.replace('start:', '"start":')
     jsonish = jsonish.replace('end:', '"end":')
--- stats/README.md
+++ stats/README.md
@@ -9,3 +9,8 @@ cargo +nightly test --features flame_it
 For details on how to expand the flame graph, see here: https://github.com/llogiq/flamer
 
 Also, note that this is currently being generated on tests, which is not as accurate as building in release mode and running it. But at least we can figure out what is slowing down our testing.
+
+To create a flame graph for release mode performance and target a specific test case for benchmarking, a command like the following can be used:
+```
+cargo +nightly test --release --features flame_it --package tss-ecdsa --lib -- protocol::tests::test_run_protocol --exact --nocapture
+```
